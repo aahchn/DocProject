3dd764d7bea250d6af4afa85c08f6ffe6db70fa5
Aaron Chan <aaronchan@aarons-mbp.attlocal.net>
2020-03-04 22:35:34 -0800

    inital commit

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w3-container w3-dark-grey">
+      <h1>Placeholder</h1> <!-- need to grab from database dynamically -->
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'Header',
+  methods:{
+  }
+};
+</script>
+
+
+<style scoped>
+div {
+  padding: 7px;
+  width: 20%  /* want to be dynamic with the text (text + 5%) */
+}
+</style>
---
+++ vue/src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w3-sidebar w3-bar-block w3-border-right" style="width:15%">
+    <a href="#" class="w3-bar-item w3-button">Stuff</a>
+    <a href="#" class="w3-bar-item w3-button">From</a>
+    <a href="#" class="w3-bar-item w3-button">Database</a>
+  </div>
+</template>
+
+<style>
+</style>
+
+
+<script>
+</script>
