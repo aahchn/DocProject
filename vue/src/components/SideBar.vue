be2fac83fea17065fc2f3415e8ebd71c98e3bc31
Aaron Chan <aaronchan@aarons-mbp.attlocal.net>
2020-03-05 21:24:32 -0800

    updated some of Sidebar

--- vue/src/components/SideBar.vue
+++ vue/src/components/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w3-sidebar w3-bar-block w3-border-right" style="width:15%">
-    <a href="#" class="w3-bar-item w3-button">Stuff</a>
-    <a href="#" class="w3-bar-item w3-button">From</a>
-    <a href="#" class="w3-bar-item w3-button">Database</a>
+    <a href="#" class="w3-bar-item w3-button">Python</a>
+    <a href="#" class="w3-bar-item w3-button">Java</a>
+    <a href="#" class="w3-bar-item w3-button">JavaScript</a>
   </div>
 </template>
 
@@ -11,4 +11,20 @@
 
 
 <script>
+// TODO
+import axios from 'axios'
+export default{
+  data(){
+
+  }
+},
+computed:{
+  getNameFromDatabase(){
+    const path = `http://127.0.0.1:5000/item` //TODO: talking to backend (flask)
+    axios.get(path)
+    .then(response => {
+
+    })
+  }
+}
 </script>
